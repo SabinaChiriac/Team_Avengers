946fe4adc7d770d21983b369a02149c98b65b3cc
andreea_arvatu <arvatuandreea@yahoo.com>
2021-10-29 11:02:05 +0300

    Added classes for employees and orders.

---
+++ Adopt_a_new_friend/Adopt_a_new_friend/Models/EntityLayer/Employee.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adopt_a_new_friend.Models.EntityLayer
+{
+    class Employee : BasePropertyChanged
+    {
+        private int? employee_code;
+        public int? Employee_code
+        {
+            get
+            {
+                return employee_code;
+            }
+            set
+            {
+                employee_code = value;
+                NotifyPropertyChanged("Employee_code");
+            }
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        private string phone;
+        public string Phone
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                phone = value;
+                NotifyPropertyChanged("Phone");
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+                NotifyPropertyChanged("Email");
+            }
+        }
+
+        private int? id_address;
+        public int? Id_address
+        {
+            get
+            {
+                return id_address;
+            }
+            set
+            {
+                id_address = value;
+                NotifyPropertyChanged("Id_address");
+            }
+        }
+    }
+}
