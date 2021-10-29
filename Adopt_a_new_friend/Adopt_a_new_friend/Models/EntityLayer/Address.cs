cf911026ce2e778fc9f12ee41160fdf4d83ad1fa
andreea_arvatu <arvatuandreea@yahoo.com>
2021-10-29 10:46:12 +0300

    Added Address class and BasePropertyChanged class.

---
+++ Adopt_a_new_friend/Adopt_a_new_friend/Models/EntityLayer/Address.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adopt_a_new_friend.Models.EntityLayer
+{
+    class Address : BasePropertyChanged
+    {
+        private int? id_address;
+        public int? Id_address
+        {
+            get
+            {
+                return id_address;
+            }
+            set
+            {
+                id_address = value;
+                NotifyPropertyChanged("Id_address");
+            }
+        }
+
+        private string city;
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+                NotifyPropertyChanged("City");
+            }
+        }
+
+        private string street;
+        public string Street
+        {
+            get
+            {
+                return street;
+            }
+            set
+            {
+                street = value;
+                NotifyPropertyChanged("Street");
+            }
+        }
+
+        private string number;
+        public string Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+                NotifyPropertyChanged("Number");
+            }
+        }
+    }
+}
---
+++ Adopt_a_new_friend/Adopt_a_new_friend/Models/EntityLayer/BasePropertyChanged.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+
+namespace Adopt_a_new_friend.Models.EntityLayer
+{
+    class BasePropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
