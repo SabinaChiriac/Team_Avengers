bacf47e478aa54eb852ba152f55270c08911a30f
SabinaChiriac <chiriac.sabina@yahoo.com>
2021-10-30 17:01:28 +0300

    Added Pet class in EntityLayer and modified the main window

---
+++ Adopt_a_new_friend/Adopt_a_new_friend/Models/EntityLayer/Pet.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adopt_a_new_friend.Models.EntityLayer
+{
+    class Pet : BasePropertyChanged
+    {
+        private int? id_pet;
+        public int? Id_pet
+        {
+            get
+            {
+                return id_pet;
+            }
+            set
+            {
+                id_pet = value;
+                NotifyPropertyChanged("ID Pet");
+            }
+        }
+        private String name;
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                NotifyPropertyChanged("Name of the pet");
+            }
+        }
+        private String bread;
+        public String Bread
+        {
+            get
+            {
+                return bread;
+            }
+            set
+            {
+                bread = value;
+                NotifyPropertyChanged("Pet bread");
+            }
+        }
+        private int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+                NotifyPropertyChanged("Age of the pet");
+            }
+        }
+        private String description;
+        public String Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+                NotifyPropertyChanged("Pet description");
+            }
+        }
+        private String type;
+        public String Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+                NotifyPropertyChanged("Pet type");
+            }
+        }
+    }
+}
