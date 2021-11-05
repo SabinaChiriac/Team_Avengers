bdef3aebdf7a7a59c9a21a2d280f75e1c33ae789
SabinaChiriac <chiriac.sabina@yahoo.com>
2021-11-05 10:22:53 +0200

    update project structure and added new classes

---
+++ Adopt_a_new_friend/Adopt_a_new_friend/Models/EntityLayer/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adopt_a_new_friend.Models.EntityLayer
+{
+    class User : BasePropertyChanged
+    {
+        private int? id_user;
+        public int? Id_user
+        {
+            get
+            {
+                return id_user;
+            }
+
+        }
+       
+    }
+}
