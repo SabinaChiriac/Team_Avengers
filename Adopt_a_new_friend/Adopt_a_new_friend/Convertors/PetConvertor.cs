b09e3f81df1bca9bf93a7a99379ed465bee78312
SabinaChiriac <chiriac.sabina@yahoo.com>
2021-11-09 19:54:55 +0200

    finished pet convertor class

--- Adopt_a_new_friend/Adopt_a_new_friend/Convertors/PetConvertor.cs
+++ Adopt_a_new_friend/Adopt_a_new_friend/Convertors/PetConvertor.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Data;
+using Adopt_a_new_friend.Models.EntityLayer;
 namespace Adopt_a_new_friend.Convertors
 {
-    class PetConvertor
+    class PetConvertor IMultiValueConverter
     {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+
+           return new Pet()
+          {
+            Name = values[0].ToString(),
+            Bread = values[1].ToString(),
+            // Age=int.TryParse(values[2].ToString(), out age ),
+            Description = values[3].ToString(),
+            Type = values[4].ToString()
+
+          };
+        }
+
+    public object[] ConvertBack(object[] value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        Pet pet = value as Pet;
+        Object[] result = new object[5] { pet.Name, pet.Bread, pet.Age, pet.Description, pet.Type };
+        return result;
+
     }
+
+  }
 }
